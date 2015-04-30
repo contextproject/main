376849f1c55c42ffdfa03ebae3fc1c58ec8a3a91
Tomas Heinsohn Huala <tomas2h@hotmail.com>
2015-05-01 01:17:44 +0200

    Added comments to class

--- discover/src/main/java/snippet/CommentIntensitySeeker.java
+++ discover/src/main/java/snippet/CommentIntensitySeeker.java
@@ -3,8 +3,22 @@ package snippet;
 import java.util.Set;
 import java.util.TreeSet;
 
+/**
+ * This class returns the start time for a snippet, based on the comment intensity of that song.
+ * 
+ *
+ */
 public class CommentIntensitySeeker {
 
+  /**
+   * Generates a start time for a snippet.
+   * 
+   * @param coms
+   *          The set of comments of a given song
+   * @param duration
+   *          The duration of that given song
+   * @return a start time
+   */
   protected int getStartTime(Set<Comment> coms, int duration) {
     if (coms.isEmpty()) {
       return 0;
@@ -30,12 +44,24 @@ public class CommentIntensitySeeker {
     return start;
   }
 
+  /**
+   * Seeks the Snippet to be used of a given song with a unknown duration.
+   * 
+   * @param coms
+   *          The set of comments to use
+   * @return A TimedSnippet object
+   */
   public TimedSnippet seek(Set<Comment> coms) {
 
     return new TimedSnippet(getStartTime(coms, TimedSnippet.getDefaultDuration()));
 
   }
-
+  /**
+   * Seeks the snippet to be used of a given song with the duration known.
+   * @param coms The set of comments to use
+   * @param duration The duration of the song
+   * @return A TimedSnippet object
+   */
   public TimedSnippet seekDuration(Set<Comment> coms, int duration) {
     return new TimedSnippet(getStartTime(coms, duration), duration);
   }
