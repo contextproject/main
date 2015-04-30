bb5ba3cb946c1c41403f84eb4c1145d98ab4bf6a
Tomas Heinsohn Huala <tomas2h@hotmail.com>
2015-04-30 20:28:48 +0200

    Updates CommentIntenitySeeker.java and specifically its starttime method.
    The start time method now searches the provided set of comments to find the most provitable starting point. A set is kept for all already looked up values to provide efficiency for larger sets with lots of duplicate starting times. The format is ugly because the GitHub editor does not auto indent and there is no tab on my currently used device.

--- discover/src/main/java/snippet/CommentIntensitySeeker.java
+++ discover/src/main/java/snippet/CommentIntensitySeeker.java
@@ -1,25 +1,43 @@
 package snippet;
 
 import java.util.Set;
+import java.util.TreeSet;
 
 public class CommentIntensitySeeker {
 
-	
-	protected int getStartTime(Set<Comment> coms,int duration){
-		
-		return 0;
-	}
-	
-	public TimedSnippet seek(Set<Comment> coms){
-		
-		return new TimedSnippet(getStartTime(coms,TimedSnippet.getDefaultDuration()));
-		
-	}
-	
-	
-	
-	public TimedSnippet seekDuration(Set<Comment> coms,int duration){
-		return new TimedSnippet(getStartTime(coms,duration),duration);
-	}
-	
+  protected int getStartTime(Set<Comment> coms, int duration) {
+    if (coms.isEmpty()) {
+      return 0;
+    }
+    int start = 0;
+    int maxcount = 0;
+    Set<Integer> passed = new TreeSet<Integer>();
+    for (Comment c : coms) {
+      int count = 0;
+      if (!passed.contains(c.getTime())) {
+        for (Comment c2 : coms) {
+          if (c2.getTime() >= c.getTime() && c2.getTime() <= (c.getTime() + duration)) {
+            count++;
+          }
+        }
+        if (count > maxcount) {
+          maxcount = count;
+          start = c.getTime();
+        }
+        passed.add(c.getTime());
+      }
+    }
+    return start;
+  }
+
+  public TimedSnippet seek(Set<Comment> coms) {
+
+    return new TimedSnippet(getStartTime(coms, TimedSnippet.getDefaultDuration()));
+
+  }
+
+  public TimedSnippet seekDuration(Set<Comment> coms, int duration) {
+    return new TimedSnippet(getStartTime(coms, duration), duration);
+  }
+
 }
